943a2db41aa2937a8da381837b9e2affd20cf960
Siva Patel <siva.sankalp.patel@ibm.com>
2021-05-16 12:51:54 -0400

    add new eval scripts

---
+++ examples/research_projects/rag/eval_e2e.sh
@@ -0,0 +1,41 @@
+checkpoint="checkpoint10"
+seg=token
+task=grounding
+split=test
+score=original
+# dpr=dpr_bi_$seg
+dpr=dpr_new
+format=two
+model=rag_token
+topn=5
+sourcelen=128
+targetlen=50
+SFD=/dccstor/dialog/sfeng/projects/transformers_dialdoc
+DATA_DIR=$SFD/data_v2/dd_$task\_$seg\_$format
+KB_FOLDER=$SFD/data_v2/dd_knowledge_dataset-$seg-$dpr
+FD=/dccstor/dialog/sfeng/transformers_doc2dial/checkpoints
+MODEL_PATH=$FD/dd-$seg-$task-$sourcelen-$targetlen-$format-$dpr-$score/$checkpoint
+config=dd-$seg-$task-$sourcelen-$targetlen-$format-$dpr-$score/$checkpoint
+
+jbsub -cores 4+1 -mem 256g -queue x86_1h -require v100 \
+-out $SFD/logs_eval/eval_e2e_$config.out \
+-err $SFD/logs_eval/eval_e2e_$config.err \
+python eval_rag.py \
+--model_type rag_token \
+--scoring_func $score \
+--gold_pid_path $DATA_DIR/$split.pids \
+--passages_path $KB_FOLDER/my_knowledge_dataset \
+--index_path $KB_FOLDER/my_knowledge_dataset_hnsw_index.faiss \
+--n_docs $topn \
+--model_name_or_path $MODEL_PATH \
+--eval_mode e2e \
+--evaluation_set $DATA_DIR/$split.source \
+--gold_data_path $DATA_DIR/$split.target \
+--gold_data_mode ans \
+--eval_batch_size 15 \
+--recalculate \
+--predictions_path $SFD/results/eval-$config-e2e.txt
+
+#--eval_all_checkpoints \
+
+
---
+++ examples/research_projects/rag/eval_re.sh
@@ -0,0 +1,40 @@
+checkpoint="checkpoint10"
+seg=token
+task=grounding
+split=test
+score=original
+# dpr=dpr_bi_$seg
+dpr=dpr_new
+format=two
+model=rag_token
+topn=5
+sourcelen=128
+targetlen=50
+SFD=/dccstor/dialog/sfeng/projects/transformers_dialdoc
+DATA_DIR=$SFD/data_v2/dd_$task\_$seg\_$format
+KB_FOLDER=$SFD/data_v2/dd_knowledge_dataset-$seg-$dpr
+FD=/dccstor/dialog/sfeng/transformers_doc2dial/checkpoints
+MODEL_PATH=$FD/dd-$seg-$task-$sourcelen-$targetlen-$format-$dpr-$score/$checkpoint
+config=dd-$seg-$task-$sourcelen-$targetlen-$format-$dpr-$score/$checkpoint
+
+jbsub -cores 4+1 -mem 128g -queue x86_1h -require v100 \
+-out $SFD/logs_eval/eval_re_$config.out \
+-err $SFD/logs_eval/eval_re_$config.err \
+python eval_rag.py \
+--model_type $model \
+--scoring_func $score \
+--gold_pid_path $DATA_DIR/$split.pids \
+--passages_path $KB_FOLDER/my_knowledge_dataset \
+--index_path $KB_FOLDER/my_knowledge_dataset_hnsw_index.faiss \
+--n_docs $topn \
+--model_name_or_path  $MODEL_PATH \
+--eval_mode retrieval \
+--k 1 \
+--evaluation_set $DATA_DIR/$split.source \
+--gold_data_path $DATA_DIR/$split.titles \
+--gold_data_mode ans \
+--eval_batch_size 40 \
+--recalculate \
+--predictions_path $SFD/results/eval-$config-re.txt
+# --eval_all_checkpoints \
+
