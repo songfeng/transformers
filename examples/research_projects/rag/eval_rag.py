6e8c67c676492b198c51db408b736e89572684ab
Siva Patel <siva.sankalp.patel@ibm.com>
2021-05-16 19:22:25 -0400

    bug fix for pids

--- examples/research_projects/rag/eval_rag.py
+++ examples/research_projects/rag/eval_rag.py
@@ -229,8 +229,8 @@ def evaluate_batch_retrieval(args, rag_model, questions, old_q=None):
         all_docs = rag_model.retriever.index.get_doc_dicts(result.doc_ids)
         doc_ids = result.doc_ids
     provenance_strings = []
-    pdb.set_trace()
-    for docs in all_docs:
+
+    for i, docs in enumerate(all_docs):
         provenance = [strip_title(title) for title in docs["title"]]
         # provenance_strings.append("\t".join(provenance))
         pids = "\t".join([str(int(e)) for e in doc_ids[i]])
